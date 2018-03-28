6ba33566efd3711af259e7a4bf9208e9bafee38c
olhaRassokha <35812539+olhaRassokha@users.noreply.github.com>
2018-03-28 17:55:47 +0300

    Update init.sql

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,8 +1,9 @@
+DROP SCHEMA IF EXISTS users CASCADE;
 CREATE SCHEMA IF NOT EXISTS users;
 DROP SEQUENCE IF EXISTS user_sequance CASCADE;
 CREATE SEQUENCE user_sequance;
 CREATE TABLE users."user"(
-  id bigint NOT NULL PRIMARY KEY DEFAULT nextval('category_sequance'),
+  id bigint NOT NULL PRIMARY KEY DEFAULT nextval('user_sequance'),
   username VARCHAR(20),
   password VARCHAR(60)
 );
